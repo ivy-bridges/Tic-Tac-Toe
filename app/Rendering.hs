3d7564a6cb7a716b3bc3cc0d83593f24ca49760b
ivy bridges <ivy.v.bridges@gmail.com>
2022-12-21 20:29:51 -0500

    added functionality to render state of board and place marks

--- app/Rendering.hs
+++ app/Rendering.hs
@@ -3,6 +3,8 @@ module Rendering where
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import TicTacToe
+
+import Data.Array
 -- 
 
 {- gloss play function is set up as
@@ -19,12 +21,20 @@ play :: Display -- (InWindow "Tic-Tac-Toe" (500,500), (10,10))
 -- hold state of game as well as currently selected square
 type GameWorld = (GameState, (Int, Int))
 
+emptyGame :: GameState
+emptyGame = GameState ((emptyBoard 3 3), X)
+
+-- functions for checking key type
 isArrowKey :: SpecialKey -> Bool
 isArrowKey key = elem key [KeyUp,KeyDown,KeyLeft,KeyRight]
 
 isSpaceKey :: SpecialKey -> Bool
 isSpaceKey key = key == KeySpace
 
+isResetKey :: SpecialKey -> Bool
+isResetKey key = key == KeyEnter
+
+-- moves the selected square within the bounds (1,1), (3,3)
 moveSelection :: SpecialKey -> (Int, Int) -> (Int, Int)
 moveSelection key (x,y)
     | key == KeyDown    && y>1 = (x,y-1)
@@ -39,13 +49,17 @@ handleEvents :: Event -> GameWorld -> GameWorld
 handleEvents (EventKey (SpecialKey key) Down _ _) (state, selection)
     | isArrowKey key = (state, moveSelection key selection)
     | isSpaceKey key = (makeMove state selection, selection)
+    | isResetKey key = (emptyGame, selection)
 handleEvents _ game = game
 
+-- we don't need to do anything when time passes
 passTime :: Float -> GameWorld -> GameWorld
 passTime _ world = world
 
+
+-- renders the lines of the grid, the highlighted square, and the state of the game
 renderGame :: GameWorld -> Picture
-renderGame (state, (sx,sy)) = Pictures [gridLines, squareHighlight]
+renderGame (state, (sx,sy)) = Pictures [gridLines, squareHighlight, renderedBoard]
     where gridLines = Pictures $ map (Color black) [l1,l2,l3,l4]
     
           l1 = Line [(-75,-225), (-75, 225)]
@@ -58,6 +72,28 @@ renderGame (state, (sx,sy)) = Pictures [gridLines, squareHighlight]
           selectionSquare = Color selectionColor $ rectangleSolid 149 149
           
           squareHighlight = Translate squareX squareY selectionSquare
+          
+          renderedBoard = Color blue $ renderBoard (pullBoard state)
+          
+-- render a single mark
+-- x draws an x, o draws a circle      
+renderMark :: Mark -> Picture
+renderMark X = Pictures $ [Line [(-50,-50), (50,50)], Line [(-50,50), (50,-50)]]
+renderMark O = Circle 50
+
+-- render the mark a square is filled with if it has one
+renderSquare :: Square -> Picture
+renderSquare (Filled mark) = renderMark mark
+renderSquare Empty = Blank 
+
+-- renders a board by rendering and translating each of the squares
+renderBoard :: Board -> Picture
+renderBoard board = Pictures translatedSquares
+    where translations  = [(-300+150*fromIntegral(x), -300+150*fromIntegral(y)) | ((x,y),s) <- assocs board]
+    
+          drawnSquares = [renderSquare s | ((x,y),s) <- assocs board]
+          
+          translatedSquares = zipWith (\(x,y) s -> Translate x y s) translations drawnSquares
           
           
           
